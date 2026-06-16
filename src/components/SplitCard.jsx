0f5765ad426a1b7ab8c0c0c779a2bd59ee734b22
Wilson Symonds <wilsonsymonds111@gmail.com>
2026-06-16 08:08:28 +0000

    File changes

--- src/components/SplitCard.jsx
+++ src/components/SplitCard.jsx
@@ -2,7 +2,7 @@ import { MoreHorizontal } from 'lucide-react';
 import { memo } from 'react';
 
 const SPLIT_IMAGES = {
-  'upper-lower': 'https://media.base44.com/images/public/6a16b583ab0ebad6332038a3/a395a63e8_generated_image.png',
+  'upper-lower': 'https://media.base44.com/images/public/6a16b583ab0ebad6332038a3/60f426734_image.png',
   'push-pull-legs': 'https://media.base44.com/images/public/6a16b583ab0ebad6332038a3/6cb61ef90_generated_image.png',
   'full-body': 'https://media.base44.com/images/public/6a16b583ab0ebad6332038a3/2b264bebb_generated_image.png',
   'ul-ppl': 'https://media.base44.com/images/public/6a16b583ab0ebad6332038a3/ac60aca39_generated_image.png',
