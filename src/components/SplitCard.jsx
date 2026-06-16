f6b2f8fc0d66e2ce8e218badb41617f2754797af
Wilson Symonds <wilsonsymonds111@gmail.com>
2026-06-16 05:19:41 +0000

    File changes

--- src/components/SplitCard.jsx
+++ src/components/SplitCard.jsx
@@ -1,7 +1,7 @@
 import { Dumbbell, ChevronRight, MoreHorizontal } from 'lucide-react';
 
 const SPLIT_BG_IMAGES = {
-  'upper-lower': 'https://media.base44.com/images/public/6a16b583ab0ebad6332038a3/839011abd_generated_image.png',
+  'upper-lower': 'https://media.base44.com/images/public/6a16b583ab0ebad6332038a3/fa95818e7_generated_image.png',
   'push-pull-legs': 'https://media.base44.com/images/public/6a16b583ab0ebad6332038a3/799340377_generated_image.png',
   'full-body': 'https://media.base44.com/images/public/6a16b583ab0ebad6332038a3/8cc98624d_generated_image.png',
   'ul-ppl': 'https://media.base44.com/images/public/6a16b583ab0ebad6332038a3/0d40836a8_generated_image.png',
