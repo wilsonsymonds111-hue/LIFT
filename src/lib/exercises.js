726e2c977f371cde372e7c0c53636b353f9ee63d
Wilson Symonds <wilsonsymonds111@gmail.com>
2026-06-24 10:04:05 +0000

    File changes

--- src/lib/exercises.js
+++ src/lib/exercises.js
@@ -59,7 +59,7 @@ export const ALL_EXERCISES = [
   { name: 'Lat Pulldown', muscle: 'Back' },
   { name: 'Lateral Raise (Dumbbell)', muscle: 'Shoulders' },
   { name: 'Lateral Raise (Machine)', muscle: 'Shoulders' },
-  { name: 'Leg Curl (Machine)', muscle: 'Legs' },
+  { name: 'Hamstring Curl (Machine)', muscle: 'Legs' },
   { name: 'Leg Extension (Machine)', muscle: 'Legs' },
   { name: 'Leg Press', muscle: 'Legs' },
   { name: 'Leg Press Calf Raise', muscle: 'Legs' },
