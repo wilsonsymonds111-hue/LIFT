924f57f02ed1974555e8d3c74353e354f63a8cc1
Wilson Symonds <wilsonsymonds111@gmail.com>
2026-07-07 08:03:15 +0000

    File changes

--- src/components/PersonIcon.jsx
+++ src/components/PersonIcon.jsx
@@ -1,4 +1,4 @@
-const ICON_URL = "https://media.base44.com/images/public/6a16b583ab0ebad6332038a3/162f25c6e_generated_image.png";
+const ICON_URL = "https://media.base44.com/images/public/6a16b583ab0ebad6332038a3/6f33635b2_generated_image.png";
 
 export default function PersonIcon({ className, strokeWidth }) {
   return (
