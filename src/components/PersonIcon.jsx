0d469c038c24a4a8128389bd355a44518cb246fe
Wilson Symonds <wilsonsymonds111@gmail.com>
2026-07-07 08:04:16 +0000

    File changes

--- src/components/PersonIcon.jsx
+++ src/components/PersonIcon.jsx
@@ -1,12 +1,5 @@
-const ICON_URL = "https://media.base44.com/images/public/6a16b583ab0ebad6332038a3/6f33635b2_generated_image.png";
+import { UserRound } from 'lucide-react';
 
-export default function PersonIcon({ className, strokeWidth }) {
-  return (
-    <img
-      src={ICON_URL}
-      alt="Body Stats"
-      className={className}
-      style={{ objectFit: 'contain' }}
-    />
-  );
+export default function PersonIcon({ className, strokeWidth = 2 }) {
+  return <UserRound className={className} strokeWidth={strokeWidth} />;
 }
