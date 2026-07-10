faefbed171e1b4fe53ef2daf19cd615a3fffbaf0
Wilson Symonds <wilsonsymonds111@gmail.com>
2026-07-10 03:30:26 +0000

    File changes

--- src/lib/exercises.js
+++ src/lib/exercises.js
@@ -9,7 +9,7 @@ export const ALL_EXERCISES = [
   { name: 'Bicep Curl (Barbell)', muscle: 'Arms' },
   { name: 'Bicep Curl Dumbbell Supinated', muscle: 'Arms' },
   { name: 'Bulgarian Split Squat', muscle: 'Legs' },
-  { name: 'Burpee', muscle: 'Full Body' },
+  { name: 'Dumbbell Bulgarian Split Squat', muscle: 'Legs' },
   { name: 'Cable Crunch', muscle: 'Core' },
   { name: 'Bicep Cable Curl', muscle: 'Arms' },
   { name: 'Cable Fly', muscle: 'Chest' },
