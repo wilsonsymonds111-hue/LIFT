f44bd5c04e3a64b76c1d4e3d5224a36e963e66b8
Wilson Symonds <wilsonsymonds111@gmail.com>
2026-07-12 10:51:05 +0000

    File changes

--- src/components/WorkoutSheet.jsx
+++ src/components/WorkoutSheet.jsx
@@ -614,7 +614,7 @@ export default function WorkoutSheet({ template, onFinish, onSaveHistory, savedS
                         <Draggable key={exercise.name + idx} draggableId={exercise.name + idx} index={idx}>
                           {(p) => (
                             <div ref={p.innerRef} {...p.draggableProps}>
-                              <ExerciseSection key={`${exercise.name}-${idx}`} exercise={exercise} cardIndex={idx + 1} onBestSet={handleBestSet} dragHandleProps={p.dragHandleProps} exerciseImage={exerciseImages[exercise.name.toLowerCase()]} onDeleteExercise={() => handleDeleteExercise(idx)} initialState={exerciseStateRef.current[exercise.name]} onStateChange={(state) => handleExerciseStateChange(exercise.name, state)} />
+                              <ExerciseSection key={`${exercise.name}-${idx}`} exercise={exercise} onBestSet={handleBestSet} dragHandleProps={p.dragHandleProps} exerciseImage={exerciseImages[exercise.name.toLowerCase()]} onDeleteExercise={() => handleDeleteExercise(idx)} initialState={exerciseStateRef.current[exercise.name]} onStateChange={(state) => handleExerciseStateChange(exercise.name, state)} />
                             </div>
                           )}
                         </Draggable>
